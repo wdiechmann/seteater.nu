4c2390a71f43f28eb4d09656b3e3656362fc0e53
Enrique Phillips <enrique.phillips.wac@gmail.com>
2014-12-01 14:14:47 +0100

    changed files to show new curtain

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
-  before_create :create_account
+  # before_create :create_account
 
   has_many :punch_clocks
   has_many :employees
